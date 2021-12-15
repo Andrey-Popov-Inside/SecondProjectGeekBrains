171e4e2af3de733bc77a5839c71dafd74ca14d5a
geekbrains <info@geekbrains.ru>
2021-12-15 21:31:31 +0300

    Выполненая седьмая домашка

---
+++ src/SevenLesson/lesson7/MainApp.java
@@ -0,0 +1,9 @@
+package SevenLesson.lesson7;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        UserInterface userInterface = new UserInterface();
+        userInterface.runApplication();
+    }
+}
